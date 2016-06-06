043447098b6bb443b76b898d499dbde766985ac4
Andrew <andrew@purplecs.com>
2016-06-06 13:16:55 +0100

    Selectors

--- TheNetNinja/jQuery/scripts/script.js
+++ TheNetNinja/jQuery/scripts/script.js
@@ -1,3 +1,44 @@
-$(document).ready(function(){
-  alert("jQuery Loaded");
+$(document).ready(function() {
+    //alert("jQuery Loaded");
+
+    //https://api.jquery.com/category/selectors/
+    //Selector / Filters
+
+    $("header nav li:first").css({
+        border: "2px solid red"
+    });
+
+    $("header nav li:even").css({
+        border: "2px solid blue"
+    });
+
+
+
+    //Not Filters
+    $("section:not('#contact')").css({
+        border: "2px solid green"
+    });
+
+    //Less than / Greater than
+    $("#social-nav li:lt(2)").css({
+        border: "4px solid orange"
+    });
+
+    $("#social-nav li:gt(2)").css({
+        border: "4px solid pink"
+    });
+
+    //Attribute Filters
+    $("div[class]").css({
+        border: "2px solid blue"
+    });
+
+    $("img[alt]").css({
+        border: "2px solid gold"
+    });
+
+    $("img[alt=quote]").css({
+        border: "4px solid purple"
+    });
+
 });
