9009d9853b38bfe5955b321876ee714cade03709
Andrew <andrew@purplecs.com>
2016-06-06 14:13:42 +0100

    Up to Lesson #14

--- TheNetNinja/jQuery/scripts/script.js
+++ TheNetNinja/jQuery/scripts/script.js
@@ -41,4 +41,102 @@ $(document).ready(function() {
         border: "4px solid purple"
     });
 
+
+    // Chaining - Lesson 8
+    $("#contact-methods").css({
+            border: "2px solid red"
+        })
+        .next().css({
+            border: "2px solid blue"
+        })
+        .closest("section").css({
+            border: "2px solid green"
+        });
+
+    // Adding Content - Lesson 9
+
+    var tweet = "<div style='background:purple'>Some Content From Variable</div>";
+
+    //$("#tweets .tweet-data").append(tweet);
+    //$("#tweets .tweet-data").prepend(tweet);
+    //$("#tweets .tweet-data").before(tweet);
+    //$("#tweets .tweet-data").after(tweet);
+    //$("#tweets .tweet-data").html(tweet);
+    //$("#tweets .tweet-data").text(tweet);
+
+
+    // Wrapping and unwrapping Elements - Lesson 10
+
+    //$("section").wrap("<div>");
+    //$("section").unwrap();
+    //$("section").wrapAll("<div>");
+
+    var wrapper = "<div class='wrapper'>";
+    var button = $(".button");
+    var wrapped = true;
+
+    button[0].onclick = function() {
+        if (wrapped) {
+            $("section").unwrap();
+            wrapped = false;
+            button.text("Wrap");
+        } else {
+            $("section").wrapAll(wrapper);
+            wrapped = true;
+            button.text("UnWrap");
+        }
+    };
+
+    //Empty & Removing Content
+
+    //$(".button").empty();
+    //$("#points-of-sale").empty();
+
+
+    // $(".button").remove();
+    // $("#points-of-sale").remove();
+    // $("#contact img").remove();
+
+
+    //Changing & Removing Attributes
+
+    //$("#contact img").removeAttr("alt");
+    //$("#contact img").attr("alt", "Hello world!");
+
+    //Retrieve
+    console.log($("#contact img").attr("alt"));
+
+
+
+    // CSS With jquery
+    //Get current
+    console.log($("#social-nav").css("position"));
+
+    //Set css
+    $("#social-nav").css("left", "100px");
+    //Set CSS - Chaining
+    $("#social-nav").css("left", "100px").css("top", "10px");
+    //Set CSS - Object
+
+
+    // $("#social-nav").css({
+    //     height: "40px",
+    //     width: "500px",
+    //     background:"red"
+    // });
+
+    var cssObject = {
+      height: "40px",
+      width: "500px",
+      background:"red"
+    };
+
+    $("#social-nav").css(cssObject);
+
+
+    //Working With Classes
+
+
+
+
 });
