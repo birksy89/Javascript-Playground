590c464d058d5b2cd2e6ce506d66bad5a0312ed2
Andrew <andrew@purplecs.com>
2016-06-10 16:48:28 +0100

    Remaining Char Methods - Working!

--- React/ReactForDesigners/scripts.js
+++ React/ReactForDesigners/scripts.js
@@ -2,28 +2,40 @@ var TweetBox = React.createClass({
     getInitialState: function() {
         return {
             text: "",
-            remainingChars: "Remaining Characters: " + 140,
+            remainingChars:"Number of Characters Remaining",
             photoAdded: false
         }
     },
     handleChange: function(event) {
         console.log(event.target.value);
         this.setState({
-            text: event.target.value,
-            remainingChars: 140 - event.target.value.length
+            text: event.target.value
+
         });
     },
     togglePhoto: function(event) {
+
+      console.log(this.state.photoAdded);
+
         this.setState({
             photoAdded: !this.state.photoAdded
         });
+    },
+    remainingChars: function() {
+        if (this.state.photoAdded) {
+            return 140 - 23 - this.state.text.length;
+            console.log(140 - 23 - this.state.text.length);
+        } else {
+            return 140 - this.state.text.length;
+            console.log(140 - this.state.text.length);
+        }
     },
     render: function() {
         return (
             <div className="well clearfix">
                 <textarea className="form-control" onChange={this.handleChange}></textarea>
                 <br/>
-                <span>{this.state.remainingChars}</span>
+                <span>{this.remainingChars()}</span>
                 <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
                 <button className="btn btn-default pull-right" onClick={this.togglePhoto}>
                     {this.state.photoAdded
