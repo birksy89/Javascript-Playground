dc52c8a890a451176c3e7b9f695d7621a3bcd1e0
Andrew <andrew@purplecs.com>
2016-06-10 16:24:42 +0100

    Photo Added

--- React/ReactForDesigners/scripts.js
+++ React/ReactForDesigners/scripts.js
@@ -1,16 +1,22 @@
 var TweetBox = React.createClass({
     getInitialState: function() {
         return {
-          text: "",
-        remainingChars: "Remaining Characters: " + 140}
+            text: "",
+            remainingChars: "Remaining Characters: " + 140,
+            photoAdded: false
+        }
     },
     handleChange: function(event) {
         console.log(event.target.value);
         this.setState({
-          text: event.target.value,
-          remainingChars: 140 - event.target.value.length
-        }
-        );
+            text: event.target.value,
+            remainingChars: 140 - event.target.value.length
+        });
+    },
+    togglePhoto: function(event) {
+        this.setState({
+            photoAdded: !this.state.photoAdded
+        });
     },
     render: function() {
         return (
@@ -19,7 +25,11 @@ var TweetBox = React.createClass({
                 <br/>
                 <span>{this.state.remainingChars}</span>
                 <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
-
+                <button className="btn btn-default pull-right" onClick={this.togglePhoto}>
+                    {this.state.photoAdded
+                        ? "✓ Photo Added"
+                        : "Add Photo"}
+                </button>
                 <h1>{this.state.text}</h1>
             </div>
 
