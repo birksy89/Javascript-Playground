97972d9b5fae7270c9f201e9899b890b8b0de8ae
Andrew <andrew@purplecs.com>
2016-06-10 15:05:33 +0100

    reactjs-introduction-for-people-who-know-just-enough-jquery-to-get-by

---
+++ React/ReactForDesigners/scripts.js
@@ -0,0 +1,13 @@
+var TweetBox = React.createClass({
+  render: function(){
+    return(
+        <h1>Hello, world!</h1>
+    );
+
+  }
+})
+
+ReactDOM.render(
+  <TweetBox />,
+  document.getElementById('container')
+);
