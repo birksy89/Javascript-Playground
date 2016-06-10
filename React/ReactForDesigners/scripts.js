91133c994ba19bc78674d8b0819987232e743c24
Andrew <andrew@purplecs.com>
2016-06-10 16:00:39 +0100

    Remaining Characters

--- React/ReactForDesigners/scripts.js
+++ React/ReactForDesigners/scripts.js
@@ -1,14 +1,28 @@
 var TweetBox = React.createClass({
+    getInitialState: function() {
+        return {
+          text: "",
+        remainingChars: "Remaining Characters: " + 140}
+    },
     handleChange: function(event) {
         console.log(event.target.value);
+        this.setState({
+          text: event.target.value,
+          remainingChars: 140 - event.target.value.length
+        }
+        );
     },
     render: function() {
         return (
             <div className="well clearfix">
                 <textarea className="form-control" onChange={this.handleChange}></textarea>
                 <br/>
-                <button className="btn btn-primary pull-right" disabled>Tweet</button>
+                <span>{this.state.remainingChars}</span>
+                <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
+
+                <h1>{this.state.text}</h1>
             </div>
+
         );
 
     }
