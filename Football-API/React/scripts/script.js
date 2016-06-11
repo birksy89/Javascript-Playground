f2dd83269a7181bac5ad1ac3057f04f002e05000
Andrew <andy.birks@gmail.com>
2016-06-11 19:54:59 +0100

    Tidying Layout

--- Football-API/React/scripts/script.js
+++ Football-API/React/scripts/script.js
@@ -76,8 +76,22 @@ var TeamList = React.createClass({
                 {teamDataTest.map(function(team, index) {
                     return <div key={index} className="panel panel-default">
                         <div className="panel-heading">{team.name}</div>
-                        <div className="panel-body"><img className="crestImage" src={team.crestUrl}/>
-                        <PlayerList dataUrl ={team._links.players.href}/>
+                        <div className="panel-body">
+                            <div className="row">
+                                <div className="col-sm-6">
+                                    <h2>Team Details:
+                                    </h2>
+                                    <p>{team.name}</p>
+                                    <img className="crestImage pull-left" src={team.crestUrl}/>
+                                    <p>{team.code}</p>
+                                    <p>{team.squadMarketValue}</p>
+                                    <p>{team.shortName}</p>
+                                </div>
+                                <div className="col-sm-6">
+                                    <PlayerList dataUrl ={team._links.players.href}/>
+                                </div>
+                            </div>
+
                         </div>
                     </div>;
                 })}
@@ -87,66 +101,51 @@ var TeamList = React.createClass({
     }
 });
 
-
 var PlayerList = React.createClass({
 
-  getInitialState: function() {
-      return {data: []};
-  },
+    getInitialState: function() {
+        return {data: []};
+    },
 
-  componentDidMount: function() {
+    componentDidMount: function() {
 
-      this.serverRequest = $.ajax({
-          type: "GET",
-          url: this.props.dataUrl,
-          headers: {
-              'X-Auth-Token': 'ea93b0d4d7724762b54ece5762079114'
-          },
-          success: function(result) {
-              var playerData = result.players;
-              console.log(playerData);
-              this.setState({data: playerData});
-          }.bind(this),
-          error: handleError
+        this.serverRequest = $.ajax({
+            type: "GET",
+            url: this.props.dataUrl,
+            headers: {
+                'X-Auth-Token': 'ea93b0d4d7724762b54ece5762079114'
+            },
+            success: function(result) {
+                var playerData = result.players;
+                console.log(playerData);
+                this.setState({data: playerData});
+            }.bind(this),
+            error: handleError
 
-      });
+        });
 
-  },
+    },
 
-  render: function() {
+    render: function() {
 
-    var tester = this.state.data;
-    console.log(tester);
-    return (
-      <div className="playerLister">
-        <h2>Players</h2>
+        var playerArray = this.state.data;
+        console.log(playerArray);
+        return (
+            <div className="playerLister">
+                <h2>Players</h2>
 
+                {playerArray.map(function(player, index) {
+                    return <li key={index}>{player.name}
+                        - {player.jerseyNumber}</li>;
+                })}
 
-        {tester.map(function(station, index) {
-            return <li key={index}>{station.name}
-                - {index}</li>;
-        })}
-
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
     <CurrentGame/>, document.getElementById('data-panel'));
-
-function initialCall() {
-    $.ajax({
-        type: "GET",
-        url: footballURL,
-        headers: {
-            'X-Auth-Token': 'ea93b0d4d7724762b54ece5762079114'
-        },
-        success: callbackFootie,
-        error: handleError
-
-    });
-}
 
 function handleError(jqXHR, textStatus, error) {
     console.log(error);
