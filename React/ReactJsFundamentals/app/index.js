d2bd17d7dea2f079f8f90fc37823e471bb1e3a29
Andrew <andrew@purplecs.com>
2016-06-24 14:15:40 +0100

    Advanced Example

--- React/ReactJsFundamentals/app/index.js
+++ React/ReactJsFundamentals/app/index.js
@@ -14,12 +14,47 @@ var ReactDOM = require ('react-dom');
 //   <HelloWorld/>, document.getElementById('app')
 // );
 
+//Basic Example
+//------------------------------
+// var HelloUser = React.createClass({
+//   render: function(){
+//     return (
+//       <div> Hello, {this.props.name}</div>
+//     )
+//   }
+// });
+// ReactDOM.render(<HelloUser name="Tyler"/>, document.getElementById('app'));
 
-var HelloUser = React.createClass({
+
+//Advanced Example
+//------------------------------
+var FriendsContainer = React.createClass({
   render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
     return (
-      <div> Hello, {this.props.name}</div>
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
     )
   }
 });
-ReactDOM.render(<HelloUser name="Tyler"/>, document.getElementById('app'));
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<FriendsContainer/>, document.getElementById('app'));
