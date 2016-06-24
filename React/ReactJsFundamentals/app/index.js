960af958d45506cf2210f317c1197c06c9a80e9d
Andrew <andrew@purplecs.com>
2016-06-24 14:00:55 +0100

    Basic Example - Using "props"

--- React/ReactJsFundamentals/app/index.js
+++ React/ReactJsFundamentals/app/index.js
@@ -1,15 +1,25 @@
 var React = require('react');
 var ReactDOM = require ('react-dom');
 
-var HelloWorld = React.createClass({
-    render: function(){
-      return(
-        <div>Hello World - Using webpack-dev-server</div>
-      )
-    }
-})
+// var HelloWorld = React.createClass({
+//     render: function(){
+//       return(
+//         <div>Hello World - Using webpack-dev-server</div>
+//       )
+//     }
+// })
+//
+//
+// ReactDOM.render(
+//   <HelloWorld/>, document.getElementById('app')
+// );
 
 
-ReactDOM.render(
-  <HelloWorld/>, document.getElementById('app')
-);
+var HelloUser = React.createClass({
+  render: function(){
+    return (
+      <div> Hello, {this.props.name}</div>
+    )
+  }
+});
+ReactDOM.render(<HelloUser name="Tyler"/>, document.getElementById('app'));
