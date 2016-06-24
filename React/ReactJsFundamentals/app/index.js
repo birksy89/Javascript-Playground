98331e231c2d1c1d4722bf3f538fd716ce664afb
Andrew <andrew@purplecs.com>
2016-06-24 14:56:52 +0100

    Facebook Example - Doesn't work

--- React/ReactJsFundamentals/app/index.js
+++ React/ReactJsFundamentals/app/index.js
@@ -28,33 +28,64 @@ var ReactDOM = require ('react-dom');
 
 //Advanced Example
 //------------------------------
-var FriendsContainer = React.createClass({
-  render: function(){
-    var name = 'Tyler McGinnis'
-    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
-    return (
-      <div>
-        <h3> Name: {name} </h3>
-        <ShowList names={friends} />
-      </div>
-    )
-  }
-});
+// var FriendsContainer = React.createClass({
+//   render: function(){
+//     var name = 'Tyler McGinnis'
+//     var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+//     return (
+//       <div>
+//         <h3> Name: {name} </h3>
+//         <ShowList names={friends} />
+//       </div>
+//     )
+//   }
+// });
+//
+// var ShowList = React.createClass({
+//   render: function(){
+//     var listItems = this.props.names.map(function(friend){
+//       return <li> {friend} </li>;
+//     });
+//     return (
+//       <div>
+//         <h3> Friends </h3>
+//         <ul>
+//           {listItems}
+//         </ul>
+//       </div>
+//     )
+//   }
+// });
+//
+// ReactDOM.render(<FriendsContainer/>, document.getElementById('app'));
 
-var ShowList = React.createClass({
-  render: function(){
-    var listItems = this.props.names.map(function(friend){
-      return <li> {friend} </li>;
-    });
-    return (
-      <div>
-        <h3> Friends </h3>
-        <ul>
-          {listItems}
-        </ul>
-      </div>
-    )
-  }
-});
 
-ReactDOM.render(<FriendsContainer/>, document.getElementById('app'));
+// Facebook Example - Not fully working - Images don't load.
+// var ProfilePic = React.createClass({
+//   render: function() {
+//     return (
+//       <img src={'https://photo.fb.com/' + this.props.username} />
+//     )
+//   }
+// })
+// var ProfileLink = React.createClass({
+//   render: function() {
+//     return (
+//       <a href={'https://www.fb.com/' + this.props.username}>
+//         {this.props.username}
+//       </a>
+//     )
+//   }
+// })
+// var Avatar = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <ProfilePic username={this.props.username} />
+//         <ProfileLink username={this.props.username} />
+//       </div>
+//     )
+//   }
+// })
+//
+//  ReactDOM.render(<Avatar username="tylermcginnis" />, document.getElementById('app'));
