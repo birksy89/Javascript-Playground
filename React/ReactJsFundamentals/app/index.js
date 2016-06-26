dd9a80374fef4b08ff9ad18b8f7e1845b277ed17
Andrew <andy.birks@gmail.com>
2016-06-26 13:33:12 +0100

    React Router

--- React/ReactJsFundamentals/app/index.js
+++ React/ReactJsFundamentals/app/index.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var routes = require('./config/routes');
+
 // var HelloWorld = React.createClass({
 //     render: function(){
 //       return(
@@ -106,61 +108,84 @@ var ReactDOM = require('react-dom');
 //   <HelloWorld name="Andrew" anydatacangohere={29}/>, document.getElementById('app')
 // );
 
-var USER_DATA = {
-    name: "Andrew Birks",
-    username: "birksy89",
-    image: "http://avatars1.githubusercontent.com/u/4971630?v=3&s=460"
-}
+// var USER_DATA = {
+//     name: "Andrew Birks",
+//     username: "birksy89",
+//     image: "http://avatars1.githubusercontent.com/u/4971630?v=3&s=460"
+// }
+//
+// var ProfilePic = React.createClass({
+//     render: function() {
+//         return (<img src={this.props.image} style={{
+//             height: 100,
+//             width: 100
+//         }}/>)
+//     }
+// })
+//
+// var Link = React.createClass({
+//
+//   changeURL: function(){
+//     window.location.replace(this.props.href)
+//   },
+//     render: function() {
+//         return (
+//             <span style={{color:'blue', cursor: 'pointer'}}
+//               onClick={this.changeURL}>
+//                 {this.props.children}
+//             </span>
+//         )
+//     }
+// })
+//
+// var ProfileLink = React.createClass({
+//     render: function() {
+//         return (
+//             <div>
+//                 <Link href={"https://www.github.com/" + this.props.username}>
+//                     {this.props.username}
+//                 </Link>
+//             </div>
+//         )
+//     }
+// })
+//
+// var ProfileName = React.createClass({
+//     render: function() {
+//         return (
+//             <div>
+//                 {this.props.name}
+//             </div>
+//         )
+//     }
+// })
+//
+// var Avatar = React.createClass({
+//     render: function() {
+//         return (
+//             <div>
+//                 <ProfilePic image={this.props.user.image}/>
+//                 <ProfileName name={this.props.user.name}/>
+//                 <ProfileLink username={this.props.user.username}/>
+//             </div>
+//         )
+//     }
+// })
 
-var ProfilePic = React.createClass({
-    render: function() {
-        return (<img src={this.props.image} style={{
-            height: 100,
-            width: 100
-        }}/>)
-    }
-})
+// ReactDOM.render(
+//     <Avatar user={USER_DATA}/>,
+//      document.getElementById('app')
+//    );
 
-var ProfileLink = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <a href={"https://www.github.com/" + this.props.username}>{this.props.username}</a>
-            </div>
-        )
-    }
-})
-
-var ProfileName = React.createClass({
-    render: function() {
-        return (
-            <div>
-                {this.props.name}
-            </div>
-        )
-    }
-})
-
-var Avatar = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <ProfilePic image={this.props.user.image}/>
-                <ProfileName name={this.props.user.name}/>
-                <ProfileLink username={this.props.user.username}/>
-            </div>
-        )
-    }
-})
 
 ReactDOM.render(
-    <Avatar user={USER_DATA}/>
-    , document.getElementById('app')
-  );
+  routes,
+     document.getElementById('app')
+   );
 
-  //All React components should be FIRST:
+//All React components should be FIRST:
 
-  /*
+/*
     Focused
     Independent
     Reusable
