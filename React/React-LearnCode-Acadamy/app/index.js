2e0e46975b26d432bc1b1a7146ce488c7881b2b5
Andrew <andy.birks@gmail.com>
2016-07-03 18:03:28 +0100

    Basic Array Map / foreach

--- React/React-LearnCode-Acadamy/app/index.js
+++ React/React-LearnCode-Acadamy/app/index.js
@@ -2,62 +2,48 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
-var HelloWorld = React.createClass({
-    render: function(){
-      return(
-        <div>Hello World - Using webpack-dev-server</div>
-      )
-    }
-})
-
-
-ReactDOM.render(
-  <HelloWorld/>, document.getElementById('app')
-);
-
-//Basic Example
-//------------------------------
-// var HelloUser = React.createClass({
-//   render: function(){
-//     return (
-//       <div> Hello, {this.props.name}</div>
-//     )
-//   }
-// });
-// ReactDOM.render(<HelloUser name="Tyler"/>, document.getElementById('app'));
 
 //Advanced Example
 //------------------------------
-// var FriendsContainer = React.createClass({
-//   render: function(){
-//     var name = 'Tyler McGinnis'
-//     var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
-//     return (
-//       <div>
-//         <h3> Name: {name} </h3>
-//         <ShowList names={friends} />
-//       </div>
-//     )
-//   }
-// });
-//
-// var ShowList = React.createClass({
-//   render: function(){
-//     var listItems = this.props.names.map(function(friend){
-//       return <li> {friend} </li>;
-//     });
-//     return (
-//       <div>
-//         <h3> Friends </h3>
-//         <ul>
-//           {listItems}
-//         </ul>
-//       </div>
-//     )
-//   }
-// });
-//
-// ReactDOM.render(<FriendsContainer/>, document.getElementById('app'));
+var FriendsContainer = React.createClass({
+  render: function(){
+    var name = 'Tyler McGinnis'
+    var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+    return (
+      <div>
+        <h3> Name: {name} </h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+
+    var deleteItem = this.props.names.map(function(friend,index){
+      return <li> {friend} - {index +1} </li>;
+    });
+
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+
+        <ol>
+          {deleteItem}
+        </ol>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<FriendsContainer/>, document.getElementById('app'));
 
 // Facebook Example - Not fully working - Images don't load.
 //------------------------------
@@ -175,19 +161,3 @@ ReactDOM.render(
 //     <Avatar user={USER_DATA}/>,
 //      document.getElementById('app')
 //    );
-
-
-ReactDOM.render(
-  routes,
-     document.getElementById('app')
-   );
-
-//All React components should be FIRST:
-
-/*
-    Focused
-    Independent
-    Reusable
-    Small
-    Testable
-  */
