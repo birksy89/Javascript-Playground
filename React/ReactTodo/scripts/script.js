265528d5b5cc4154add84505bbbe92d84ddaa717
Andrew <andy.birks@gmail.com>
2016-07-03 15:43:19 +0100

    Basic ToDo list

---
+++ React/ReactTodo/scripts/script.js
@@ -0,0 +1,47 @@
+var TodoList = React.createClass({
+    render: function() {
+        var createItem = function(item,index) {
+            return <li key={item.id}>{item.text} {index} </li>;
+        };
+        return <ul>{this.props.items.map(createItem)}</ul>;
+    }
+});
+var TodoApp = React.createClass({
+    getInitialState: function() {
+        return {items: [], text: ''};
+    },
+    onChange: function(e) {
+        this.setState({text: e.target.value});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var nextItems = this.state.items.concat([
+            {
+                text: this.state.text,
+                id: Date.now()
+            }
+        ]);
+        var nextText = '';
+        this.setState({items: nextItems, text: nextText});
+    },
+
+    removeItem: function(){
+      
+    },
+
+    render: function() {
+        return (
+            <div>
+                <h3>TODO</h3>
+                <TodoList items={this.state.items}/>
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.onChange} value={this.state.text}/>
+                    <button>{'Add #' + (this.state.items.length + 1)}</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <TodoApp/>, document.getElementById('data-panel'));
