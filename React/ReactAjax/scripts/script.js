a1cf15012ddb648f344822fb5de47e694f976030
Andrew <andy.birks@gmail.com>
2016-07-03 15:26:21 +0100

    Delete All Button

--- React/ReactAjax/scripts/script.js
+++ React/ReactAjax/scripts/script.js
@@ -5,27 +5,32 @@ var friends = "http://rest.learncode.academy/api/andrew/friends";
 var FriendsContainer = React.createClass({
 
     getInitialState: function() {
-        return {data: [], inputUsername: "Enter Username" };
+        return {data: [], inputUsername: "Enter Username"};
     },
 
     componentDidMount: function() {
         console.log("mounted");
+        this.getUsers();
 
-        this.serverRequest = $.ajax({
-            type: "GET",
-            url: friends,
-            success: function(result) {
-                console.log(result);
-                var friendsData = result;
+    },
 
-                this.setState({data: friendsData});
-                console.log(this);
-            }.bind(this),
-            error: function(e) {
-                alert("Error: " + e);
-            }
 
-        });
+    getUsers: function(){
+      this.serverRequest = $.ajax({
+          type: "GET",
+          url: friends,
+          success: function(result) {
+              console.log(result);
+              var friendsData = result;
+
+              this.setState({data: friendsData});
+              console.log(this);
+          }.bind(this),
+          error: function(e) {
+              alert("Error: " + e);
+          }
+
+      });
     },
 
     addUser: function(inputName, inputAge) {
@@ -38,11 +43,10 @@ var FriendsContainer = React.createClass({
             },
             success: function(data) {
                 console.log("Friend added!", data); //the new item is returned with an ID
+
             }
         });
     },
-
-
 
     tester: function(input) {
         console.log("Does this fire? " + input);
@@ -54,8 +58,7 @@ var FriendsContainer = React.createClass({
     },
 
     render: function() {
-        var name = 'Tyler McGinnis'
-        var friends = ['Ean Platter', 'Murphy Randall', 'Merrick Christensen']
+
         return (
             <div>
                 <h3>
@@ -65,7 +68,7 @@ var FriendsContainer = React.createClass({
                 <input ref="inputbox" value={this.state.inputUsername} onChange={this.handleChange}></input>
 
                 <button onClick={this.addUser.bind(this, this.state.inputUsername, 45)}>Add</button>
-                <ShowList names={this.state.data}  testerThing={this.addUser()} />
+                <ShowList names={this.state.data} />
             </div>
         )
     }
@@ -73,24 +76,47 @@ var FriendsContainer = React.createClass({
 
 var ShowList = React.createClass({
 
+    deleteUser: function(userID) {
 
-  deleteUser: function(userID) {
+        $.ajax({
+            type: 'DELETE',
+            url: 'http://rest.learncode.academy/api/andrew/friends/' + userID,
+            success: function() {
+                //no data...just a success (200) status code
+                console.log('Friend Deleted Successfully!');
+            }
+        });
+    },
 
-      $.ajax({
-          type: 'DELETE',
-          url: 'http://rest.learncode.academy/api/andrew/friends/' + userID,
-          success: function() {
-              //no data...just a success (200) status code
-              console.log('Friend Deleted Successfully!');
-          }
-      });
-  },
+    tester: function(input) {
+        console.log(input);
+    },
 
+
+    deleteAll: function() {
+
+        this.props.names.map(function(friend) {
+            console.log(friend.id);
+            //this.deleteUser(friend.id);
+
+
+            $.ajax({
+                type: 'DELETE',
+                url: 'http://rest.learncode.academy/api/andrew/friends/' + friend.id,
+                success: function() {
+                    //no data...just a success (200) status code
+                    console.log('Friend Deleted Successfully!');
+                }
+            });
+
+        });
+
+    },
 
     render: function() {
         var listItems = this.props.names.map(function(friend) {
             return <li key={friend.id}>
-                {friend.name} - <button > Delete - {friend.id}  </button>
+                {friend.name} - {friend.id}
             </li>;
 
         });
@@ -99,13 +125,35 @@ var ShowList = React.createClass({
                 <h3>
                     Friends:
                 </h3>
+                <button onClick={this.deleteAll}>Delete All</button>
                 <ul>
-                    {listItems}
+
+                    <Useritem users={this.props.names}/>
                 </ul>
             </div>
         )
     }
 });
+
+
+var Useritem = React.createClass({
+
+  render: function() {
+
+    var listItems2 = this.props.users.map(function(user) {
+        return <li key={user.id}>
+            {user.name} - {user.id}
+        </li>;
+
+    });
+
+      return (
+        <div>
+{listItems2}
+  </div>
+      )
+  }
+  });
 
 ReactDOM.render(
     <FriendsContainer/>, document.getElementById('data-panel'));
